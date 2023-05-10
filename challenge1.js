b10b3afd0dd233e8ef60b92274de10bb647fb099
Abdullah Fahad <iamabdullahkw@gmail.com>
2023-05-10 14:37:36 +0300

    functions a > b

--- challenge1.js
+++ challenge1.js
@@ -3,6 +3,10 @@
  * Create a function named `printName`
  * - that just prints your name on the screen
  */
+function PrintName() {
+    consle.log ("Abdullah Fahad") ;
+}
+printAge() 
 
 /**
  * Task 2:
@@ -11,6 +15,13 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+function printAge (birthyear) {
+console.log (27)
+console.log(2023 - birthyear) ;
+
+} 
+printAge(1995) 
+
 
 /**
  * Task 3:
@@ -22,10 +33,33 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
+function printHello (name, language) {
+
+   printHello ("MerhabaName");
+
+}
+ 
+console.log ("Merhaba Name")
+console.log ("Hello NAME")
+console.log ("Bonjour NAME")
+console.log ("Hola NAME")
 
 /**
+ * 
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+
+function printMax (a, b) {
+ if(a > b){
+    return a;
+ }else{
+    return b;
+ }
+}
+
+console.log (printMax(85,80));
+
+
